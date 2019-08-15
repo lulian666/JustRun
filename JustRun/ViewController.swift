e49cc653579f878fe2140c493366b8b0c0f29098
lulian666 <644139490@qq.com>
2019-08-15 14:56:49 +0800

    init

---
+++ JustRun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustRun
+//
+//  Created by lulian on 2019/8/15.
+//  Copyright © 2019 LULIANAPP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
